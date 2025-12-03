b0d8213835c37b7334e550c9bd3d4b7527f38056
Mrdeath1507 <mrdead1507@gmail.com>
2025-12-03 15:23:03 -0300

    Resolver conflictos en routers/users.py

--- routers/users.py
+++ routers/users.py
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-from fastapi import APIRouter, Depends
-from sqlalchemy.orm import Session
-from ..database import get_db
-from .. import models, schemas, auth
-
-router = APIRouter(
-    prefix="/users",
-    tags=["Users"]
-)
-
-@router.post("/register")
-def register_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    new_user = auth.create_user(user, db)
-
-    # Crear token igual que en login
-    access_token = auth.create_access_token({"sub": new_user.email})
-
-    return {
-        "user": {
-            "id": new_user.id,
-            "email": new_user.email
-        },
-        "access_token": access_token,
-        "token_type": "bearer"
-    }
-
-@router.post("/login")
-def login(credentials: schemas.UserLogin, db: Session = Depends(get_db)):
-    return auth.login(credentials, db)
-
-@router.get("/me", response_model=schemas.UserResponse)
-def get_current_user_data(current_user: models.User = Depends(auth.get_current_user)):
-    return current_user
-=======
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from database import get_db
@@ -43,6 +8,7 @@ router = APIRouter(
     tags=["Users"]
 )
 
+
 @router.post("/register")
 def register_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     new_user = auth.create_user(user, db)
@@ -59,12 +25,12 @@ def register_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
         "token_type": "bearer"
     }
 
+
 @router.post("/login")
 def login(credentials: schemas.UserLogin, db: Session = Depends(get_db)):
     return auth.login(credentials, db)
 
+
 @router.get("/me", response_model=schemas.UserResponse)
 def get_current_user_data(current_user: models.User = Depends(auth.get_current_user)):
     return current_user
-
->>>>>>> 706d10440f265cf7ae62f1ffaba147ebb1bd4b16
