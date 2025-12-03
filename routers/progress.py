321adf765191c78f8833cc216a56bb4498af4a92
Mrdeath1507 <mrdead1507@gmail.com>
2025-12-03 15:24:52 -0300

    Resolver conflictos en routers progress y workouts

--- routers/progress.py
+++ routers/progress.py
@@ -1,46 +1,3 @@
-<<<<<<< HEAD
-from fastapi import APIRouter, Depends
-from sqlalchemy.orm import Session
-from ..database import get_db
-from .. import models, schemas
-
-router = APIRouter(
-    prefix="/progress",
-    tags=["Progress"]
-)
-
-@router.post("/")
-def add_progress(record: schemas.ProgressMetricCreate, db: Session = Depends(get_db)):
-    new_record = models.Progress(**record.dict())
-    db.add(new_record)
-    db.commit()
-    db.refresh(new_record)
-    return new_record
-
-@router.get("/{user_id}")
-def get_progress(user_id: int, db: Session = Depends(get_db)):
-    return db.query(models.Progress).filter(models.Progress.user_id == user_id).all()
-
-@router.get("/top3")
-def get_top3(db: Session = Depends(get_db)):
-    users = db.query(models.User).all()
-
-    ranking = []
-
-    for user in users:
-        total_weight = 0
-        for w in user.workouts:
-            total_weight += w.weight_lifted
-
-        ranking.append({
-            "username": user.username,
-            "total_weight": total_weight
-        })
-
-    ranking_sorted = sorted(ranking, key=lambda x: x["total_weight"], reverse=True)
-
-    return ranking_sorted[:3]
-=======
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from database import get_db
@@ -82,4 +39,3 @@ def get_top3(db: Session = Depends(get_db)):
     ranking_sorted = sorted(ranking, key=lambda x: x["total_weight"], reverse=True)
 
     return ranking_sorted[:3]
->>>>>>> 706d10440f265cf7ae62f1ffaba147ebb1bd4b16
