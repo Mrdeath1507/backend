51f632c69065f1696748fafa77108d8ad1d1e909
Mrdeath1507 <mrdead1507@gmail.com>
2025-12-03 14:43:05 -0300

    Resolver conflictos de merge en database.py

--- database.py
+++ database.py
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-engine = create_engine(DATABASE_URL)
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-=======
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -35,9 +11,6 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if DATABASE_URL is None:
     raise ValueError("❌ DATABASE_URL no está definida en las variables de entorno")
 
-# ❗ ELIMINADO → era solo para Render
-# DATABASE_URL = DATABASE_URL + "?sslmode=require"
-
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -50,4 +23,3 @@ def get_db():
         yield db
     finally:
         db.close()
->>>>>>> 706d10440f265cf7ae62f1ffaba147ebb1bd4b16
